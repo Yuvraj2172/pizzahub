2ba09352beeb33cf736bb410675625ce5b9e97e6
Yuvraj Soni <demonsroar2172@gmail.com>
2023-09-18 18:16:23 +0530

    pizza app done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./index.css";
 import React from "react";
 import { ReactDOM } from "react-dom";
 const pizzaData = [
@@ -49,21 +49,83 @@ const pizzaData = [
 function App() {
   return (
     <>
-      <h1>Hello react</h1>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Header />
+      <Menu />
+      <Footer />
     </>
   );
 }
 
-function Pizza() {
+const Menu = () => {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
   return (
-    <div>
-      <img src="../pizzas/spinaci.jpg" alt="pizza" />
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella</p>
+    <main className="menu">
+      <h2>Our Menu</h2>
+      {numPizzas > 0 ? (
+        <ul className="pizzas">
+          {pizzas.map((item, index) => {
+            return (
+              <Pizza
+                key={index}
+                name={item.name}
+                ingredients={item.ingredients}
+                price={item.price}
+                photoName={item.photoName}
+                soldOut={item.soldOut}
+              />
+            );
+          })}
+        </ul>
+      ) : (
+        <p>We're still working on our menu! please come back later...</p>
+      )}
+    </main>
+  );
+};
+function Pizza(props) {
+  const { photoName, name, ingredients, price, soldOut } = props;
+  return (
+    <li className={`pizza ${soldOut ? "sold-out" : ""}`}>
+      <img src={photoName} alt="pizza" />
+      <div>
+        <h3>{name}</h3>
+        <p>{ingredients}</p>
+        <span>{soldOut ? "Sold out" : price}</span>
+      </div>
+    </li>
+  );
+}
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza CO.</h1>
+    </header>
+  );
+};
+
+const Footer = (props) => {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <Order closeHour={closeHour} openHour={openHour} />
+      ) : (
+        <p>Sorry, We're close!</p>
+      )}
+    </footer>
+  );
+};
+function Order({ closeHour, openHour }) {
+  return (
+    <div className="order">
+      <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+      <button className="btn">Order</button>
     </div>
   );
 }
--- src/Footer.jsx
+++
@@ -1,7 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className='footer' >This is the footer</footer>
-  )
-}
-
-export default Footer
